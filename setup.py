e55806aded35afdb5809714667f57f14f0ef2ce5
Nathan Hoad <nathan@hoad.io>
2019-08-11 20:03:07 -0400

    Version bump.

--- setup.py
+++ setup.py
@@ -7,12 +7,14 @@ requires = [
 setup(
     name="flake8-unused-arguments",
     license="MIT",
-    version="0.0.2",
+    version="0.0.3",
     description="flake8 extension to warn on unused function arguments",
     author="Nathan Hoad",
     author_email="nathan@hoad.io",
     py_modules=["flake8_unused_arguments"],
     url="https://github.com/nhoad/flake8-unused-arguments",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     install_requires=requires,
     entry_points={
         "flake8.extension": ["U10 = flake8_unused_arguments:Plugin"],
